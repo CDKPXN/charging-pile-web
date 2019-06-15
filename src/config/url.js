c3e5853fa9bf028c0d4cd6303609ba43773a6c3e
wslin19 <wslin18@sina.com>
2019-06-15 16:29:08 +0800

    测试

--- src/config/url.js
+++ src/config/url.js
@@ -1,7 +1,7 @@
 
 //const localSrc= "http://106.75.173.82:8082"
-const localSrc= "http://192.168.0.127:8082"
-//  const localSrc="http://192.168.0.117:8082"
+//const localSrc= "http://192.168.0.127:8084"
+  const localSrc="http://192.168.0.117:8082"
 // const localSrc= "http://localhost:8082"
 // const localSrc="http://192.168.0.156:8082"
 // const localSrc = "http://106.75.166.143:8082"
