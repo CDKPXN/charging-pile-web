10209f9ff7103d0962ac1269c146cfda9f62d5d8
lideshengai@163.com <lds3421597>
2019-06-15 16:38:32 +0800

    test

--- src/config/url.js
+++ src/config/url.js
@@ -1,6 +1,6 @@
 
 //const localSrc= "http://106.75.173.82:8082"
-const localSrc= "http://192.168.0.127:8083"
+const localSrc= "http://192.168.0.127:8082"
 //  const localSrc="http://192.168.0.117:8082"
 // const localSrc= "http://localhost:8082"
 // const localSrc="http://192.168.0.156:8082"
